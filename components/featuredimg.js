5c4f40c5d59f170080a18340dbf3158bd6305549
SyedMuzammil2212 <syedsam2212@gmail.com>
2025-04-23 03:40:36 +0530

    Small Change

---
+++ components/featuredimg.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Featuredimg = ({ src, number, text }) => {
+    return (
+        <div className=' h-[350px] w-[330px] ' >
+
+
+        </div>
+    )
+}
+
+export default Featuredimg
